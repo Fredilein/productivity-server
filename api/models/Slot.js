46541901aed298ece4cec8df056cb3b3cec6abdf
Adrian Zanga <adrian.zanga@icloud.com>
2021-05-11 12:06:31 +0200

    slot model to custom time format

--- api/models/Slot.js
+++ api/models/Slot.js
@@ -1,15 +1,20 @@
 let mongoose = require('mongoose');
 
+/*
+ *  dayjs is probably the best date/time package but is still tedious
+ *  to only work with time. So start_time and end_time is currently stored
+ *  in a custom object. Maybe better to later in the code convert it to a dayjs object.
+ */
 let slotSchema = mongoose.Schema({
-  category: {
-    type: String,
-    required: true
+  category: { type: String, required: true },
+  day: { type: Number, required: true },
+  start_time: {
+    hour: Number,
+    minute: Number
   },
-  start_date: {
-    type: Object
-  },
-  end_date: {
-    type: OBject
+  end_time: {
+    hour: Number,
+    minute: Number
   }
 });
 
